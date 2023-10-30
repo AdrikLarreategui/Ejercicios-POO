db55b6faa5ca29b39fab4e3d50d4f7243b35b58c
AdrikLarreategui <adrian15186@hotmail.com>
2023-10-30 20:11:39 +0100

    commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 class Persona {
-    constructor (name, age, gender ) {
+    constructor (name, age, gender) {
         this.name = name
         this.age = age
         this.gender = gender
@@ -10,4 +10,32 @@ class Persona {
 }
  const guy = new Persona ('Peio', 28, "male")
  guy.obtDetalles ()
- console.log (guy)
+ //console.log (guy)
+
+ class Student extends Persona {
+    constructor (name, age, gender, grade, group) {
+        super (name, age, gender)
+            this.grade = grade
+            this.group = group
+    }
+        registrar = () => {
+        return "detalles de estudiante"
+        }
+ }
+ const pupil = new Student ('Sandra', 20, "female", 3, "A")
+ pupil.registrar ()
+ //console.log (pupil)
+
+ class Teacher extends Persona {
+    constructor (name, age, gender, subject, level) {
+        super (name, age, gender)
+            this.subject = subject
+            this.level = level
+    }
+    asignar = () => {
+        return "detalles de profesor/a"
+    }
+ }
+ const teacher = new Teacher ('Katalin', 30, "female", "Anthropology", 5) 
+ teacher.asignar ()
+ console.log(teacher)
